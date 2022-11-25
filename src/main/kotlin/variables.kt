a92202c4797c6ac8d437381b392d8655dcf80f90
paras arora <parasarora1224@gmail.com>
2022-11-25 18:31:41 +0530

    Data types and variables

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+// value can be reassigned in var
+    var message = "Hello"
+    message = "Hello World!"
+    println("${message}")
+
+  // value can't be reassigned in  val
+    // val is like const
+    val  message2 = "Hello"
+    println("$message2")
+
+    // Data Type Variables
+
+    var number : Int = 40
+     var message3 : String = "message"
+    var isTrue : Boolean =false
+  
+}
